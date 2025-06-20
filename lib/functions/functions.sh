323c06342f71847dda3fede564b90b1f4b659da1
rick <riccardoguadagnini7@gmail.com>
2025-06-20 15:35:36 +0200

    Adding a filter for tanks by nation, tier and class

--- lib/functions/functions.sh
+++ lib/functions/functions.sh
@@ -18,17 +18,23 @@ function selectorMods() {
   local category="$2"
   declare -A links
 
-  selected=$(jq -r --arg category "$category" '.[$category][] | .name as $element | .mods[] | .name as $modName | "\($element), \($modName)"' "$file" | gum choose --ordered --no-limit --header "Select some mods")
-  while IFS=',' read -r element modName; do
-    element=$(echo "$element" | xargs)
-    modName=$(echo "$modName" | xargs)
+  if [[ "$category" == "Tanks" ]]; then
+    filter
+  else
+    selected=$(jq -r --arg category "$category" '.[$category][] | .name as $element | .mods[] | .name as $modName | "\($element), \($modName)"' "$file" | gum choose --ordered --no-limit --header "Select some mods")
+  fi
 
-    link=$(jq -r --arg category "$category" --arg element "$element" --arg modName "$modName" --arg downloadLink "$platform" '.[$category][] | select(.name == $element) | .mods[] | select(.name == $modName) | .[$downloadLink]' "$file")
-
-    links["$element,$modName"]="$link"
-  done <<< "$selected"
-
-  downloader
+  if [ -n "$selected" ]; then
+    while IFS=',' read -r element modName; do
+      element=$(echo "$element" | xargs)
+      modName=$(echo "$modName" | xargs)
+      link=$(jq -r --arg category "$category" --arg element "$element" --arg modName "$modName" --arg downloadLink "$platform" '.[$category][] | select(.name == $element) | .mods[] | select(.name == $modName) | .[$downloadLink]' "$file")
+      links["$element,$modName"]="$link"
+    done <<<"$selected"
+    downloader
+  else
+    return
+  fi
 }
 
 function selectorOriginal() {
@@ -43,7 +49,7 @@ function selectorOriginal() {
     readarray -t ogFiles < <(jq -r --arg category "$category" --arg name "$elem" '.[$category][] | select(.name == $name) | .backupItems[]' "$file")
 
     for fileName in "${ogFiles[@]}"; do
-      gum spin -s "minidot" --title "Restoring stock files for $elem..."  -- rsync -a --include="*/" --include="*${fileName}*/" --include="*${fileName}*" --include="${fileName}/**" --exclude="*" "$backupDir" "$destDir"
+      gum spin -s "minidot" --title "Restoring stock files for $elem..." -- rsync -a --include="*/" --include="*${fileName}*/" --include="*${fileName}*" --include="${fileName}/**" --exclude="*" "$backupDir" "$destDir"
     done
 
     gum format -t emoji "$elem's original files applied :heavy_check_mark:"
@@ -91,4 +97,22 @@ function installer() {
   gum spin -s "minidot" --title "Backing up..." -- rsync -au "$backup_dir/" "$wotbBackup"
   rm -rf "$backup_dir"
   gum format -t emoji "$modName installed :white_check_mark:"
+}
+
+function filter() {
+  ans=$(echo -e "Nation\nTier\nClass" | gum choose --header="Filter by")
+  case $ans in
+  "Nation")
+    nation=$(echo -e "$nations" | gum choose --header="Choose a nation")
+    selected=$(jq -r --arg nation "$nation" '.Tanks[] | select(.nation == $nation) | .name as $tankName | .mods[]?.name as $modName | "\($tankName), \($modName)"' tanks.json | gum choose --ordered --no-limit --header "Select some $nation tanks")
+    ;;
+  "Tier")
+    tier=$(echo -e "$tiers" | gum choose --header="Choose a tier")
+    selected=$(jq -r --arg tier "$tier" '.Tanks[] | select(.tier == $tier) | .name as $tankName | .mods[]?.name as $modName | "\($tankName), \($modName)"' tanks.json | gum choose --ordered --no-limit --header "Select some tier $tier tanks")
+    ;;
+  "Class")
+    class=$(echo -e "$classes" | gum choose --header="Choose a class")
+    selected=$(jq -r --arg class "$class" '.Tanks[] | select(.class == $class) | .name as $tankName | .mods[]?.name as $modName | "\($tankName), \($modName)"' tanks.json | gum choose --ordered --no-limit --header "Select some $class tanks")
+    ;;
+  esac
 }
