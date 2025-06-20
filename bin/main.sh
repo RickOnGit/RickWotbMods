f447e9d83cccf7b6ced980455bafc52e423f0789
rick <riccardoguadagnini7@gmail.com>
2025-06-20 15:36:05 +0200

    Adding Backup folder creation if deleted

--- bin/main.sh
+++ bin/main.sh
@@ -4,30 +4,35 @@ source /opt/RickWotbMods/lib/env/game.env
 source /opt/RickWotbMods/lib/env/user.env
 source /opt/RickWotbMods/lib/functions/functions.sh
 
+if [ ! -d "$wotbBackup" ]; then
+  mkdir -p "$wotbBackup"
+fi
+
 while true; do
   ans=$(echo -e "$menu" | gum choose --header "Select a category to mod 👇")
 
   case "$ans" in
-    "Tanks")
-      installOrBackup "$tanksFile" "$ans" "$wotbTanksBackup" "$wotbTanksData"
-      ;;
-    "Sounds")
-      installOrBackup "$soundsFile" "$ans"
-      ;;
-    "Hangars")
-      installOrBackup "$hangarsFile" "$ans"
-      ;;
-    "UI")
-      installOrBackup "$uiFile" "$ans"
-      ;;
-    "Maps")
-      installOrBackup "$mapsFile" "$ans"
-      ;;
-    "Sights")
-      installOrBackup "$sightsFile" "$ans"
-      ;;
-    "Quit")
-      clear; break
-      ;;
+  "Tanks")
+    installOrBackup "$tanksFile" "$ans" "$wotbTanksBackup" "$wotbTanksData"
+    ;;
+  "Sounds")
+    installOrBackup "$soundsFile" "$ans"
+    ;;
+  "Hangars")
+    installOrBackup "$hangarsFile" "$ans"
+    ;;
+  "UI")
+    installOrBackup "$uiFile" "$ans"
+    ;;
+  "Maps")
+    installOrBackup "$mapsFile" "$ans"
+    ;;
+  "Sights")
+    installOrBackup "$sightsFile" "$ans"
+    ;;
+  "Quit")
+    clear
+    break
+    ;;
   esac
 done
