a389945965758d21e79e2fc76c3c8581f95e5d02
rick <riccardoguadagnini7@gmail.com>
2025-06-24 13:28:41 +0200

    Adding updater

---
+++ lib/extra/update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function update() {
+  cd /opt/RickWotbMods/
+
+  gum spin -s "minidot" --title "Checking for updates..." -- git fetch
+
+  if [ "$(git rev-parse HEAD)" != "$(git rev-parse @{u})" ]; then
+    gum spin -s "minidot" --title "Updating..." -- git pull
+    gum format -t emoji "Successfully updated :heavy_check_mark:"
+    sleep 2
+  else
+    gum format -t emoji "Up to date :heavy_check_mark:"
+    sleep 2
+  fi
+
+  cd "$HOME";
+}
