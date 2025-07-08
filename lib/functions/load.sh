d6be747b8a5f7293642ff573a62e7324d7385edd
rick <riccardoguadagnini7@gmail.com>
2025-07-08 11:48:12 +0200

    Adding the load function

---
+++ lib/functions/load.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function load() {
+  set -a
+
+  for file in /opt/RickWotbMods/lib/env/*.env; do
+    [[ -f "$file" ]] && source "$file"
+  done
+
+  for file in /opt/RickWotbMods/lib/functions/*.sh; do
+    [[ -f "$file" ]] && source "$file"
+  done
+
+  for file in /opt/RickWotbMods/lib/extra/*.sh; do
+    [[ -f "$file" ]] && source "$file"
+  done
+
+  set +a
+}
