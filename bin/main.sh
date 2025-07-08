cb5fe0afadbb921424bda9bad1421d1379f5a0c3
rick <riccardoguadagnini7@gmail.com>
2025-07-08 11:46:56 +0200

    Updating setup and main

--- bin/main.sh
+++ bin/main.sh
@@ -1,24 +1,9 @@
 #!/usr/bin/env bash
 
-function load() {
-  set -a
-
-  for file in lib/env/*.env; do
-    [[ -f "$file" ]] && source "$file"
-  done
-
-  for file in lib/functions/*.sh; do
-    [[ -f "$file" ]] && source "$file"
-  done
-
-  for file in lib/extra/*.sh; do
-    [[ -f "$file" ]] && source "$file"
-  done
-
-  set +a
-}
+source /opt/RickWotbMods/lib/functions/load.sh
 
 load
 update
 userCheck
+backupDir
 mainMenu
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-source ../lib/env/game.env
-
-mkdir -p "$wotbBackup"
-
 sudo cp rickmodder /usr/local/bin
 
 sudo mv ~/RickWotbMods /opt 2>/dev/null
