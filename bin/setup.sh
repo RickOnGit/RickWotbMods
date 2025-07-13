4c4d2b44e4bc7c820b2e1acc3a654affab5be546
rick <riccardoguadagnini7@gmail.com>
2025-07-13 12:40:04 +0200

    Editing setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,15 +1,37 @@
 #!/usr/bin/env bash
 
-sudo mv ~/RickWotbMods /opt 2>/dev/null
+function baseSetup() {
+  sudo mkdir -p /usr/local/bin /opt
+  sudo mv ~/RickWotbMods /opt 2>/dev/null
 
-SCRIPT_DIR="/opt/RickWotbMods/bin"
-BASH_PATH="#!/usr/bin/env bash"
+  SCRIPT_DIR="/opt/RickWotbMods/bin"
+  BASH_PATH="#!/usr/bin/env bash"
 
-cat <<EOF >"$SCRIPT_DIR/rickmodder"
-$BASH_PATH
-bash "$SCRIPT_DIR/main.sh"
-EOF
+  cat <<EOF >"$SCRIPT_DIR/rickmodder"
+  $BASH_PATH
+  bash "\$SCRIPT_DIR/main.sh"
+  EOF
 
-chmod +x "$SCRIPT_DIR/rickmodder"
+  chmod +x "$SCRIPT_DIR/rickmodder"
+  sudo cp "$SCRIPT_DIR/rickmodder" /usr/local/bin
+}
 
-sudo cp "$SCRIPT_DIR/rickmodder" /usr/local/bin
+function macosSetup() {
+  if [ ! -d "/opt/homebrew" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  brew install bash rsync jq gum
+
+  if [ ! -f "$HOME/.zshrc" ]; then
+    touch "$HOME/.zshrc"
+    echo 'export PATH="/opt/homebrew/bin:$PATH"' >>"$HOME/.zshrc"
+    source "$HOME/.zshrc"
+  fi
+}
+
+baseSetup
+
+if [ "$OSTYPE" != "linux-gnu" ]; then
+  macosSetup
+fi
--- lib/extra/platform.sh
+++ lib/extra/platform.sh
@@ -5,13 +5,13 @@ function userInfo() {
   ans2=$(echo -e "$clients" | gum choose)
   if [[ "$ans1" == "MacOs" && "$ans2" == "WG" ]]; then
     echo "client=pc_wg" >>/opt/RickWotbMods/lib/env/user.env
-    echo "wotbData=\"$HOME/Library/Application Support/Steam/steamapps/common/World of Tanks Blitz/World of Tanks Blitz.app/Contents/Resources/Data/\"" >>/opt/RickWotbMods/lib/env/user.env
-    echo "wotbBackup=\"$HOME/Library/Application Support/Steam/steamapps/common/World of Tanks Blitz/World of Tanks Blitz.app/Contents/Resources/Backup/\"" >>/opt/RickWotbMods/lib/env/user.env
+    echo 'wotbData="$HOME/Library/Application Support/Steam/steamapps/common/World of Tanks Blitz/World of Tanks Blitz.app/Contents/Resources/Data/"' >>/opt/RickWotbMods/lib/env/user.env
+    echo 'wotbBackup="$HOME/Library/Application Support/Steam/steamapps/common/World of Tanks Blitz/World of Tanks Blitz.app/Contents/Resources/Backup/"' >>/opt/RickWotbMods/lib/env/user.env
   elif [[ "$ans1" == "Linux" && "$ans2" == "WG" ]]; then
     echo "client=pc_wg" >>/opt/RickWotbMods/lib/env/user.env
-    echo "wotbData=\"$HOME/.local/share/Steam/steamapps/common/World of Tanks Blitz/Data/\"" >>/opt/RickWotbMods/lib/env/user.env
-    echo "wotbBackup=\"$HOME/.local/share/Steam/steamapps/common/World of Tanks Blitz/Backup/\"" >>/opt/RickWotbMods/lib/env/user.env
-    echo "wotbPacks=\"$HOME/.local/share/Steam/steamapps/compatdata/444200/pfx/drive_c/users/steamuser/AppData/Local/wotblitz/packs/\"" >>/opt/RickWotbMods/lib/env/user.env
+    echo 'wotbData="$HOME/.local/share/Steam/steamapps/common/World of Tanks Blitz/Data/"' >>/opt/RickWotbMods/lib/env/user.env
+    echo 'wotbBackup="$HOME/.local/share/Steam/steamapps/common/World of Tanks Blitz/Backup/"' >>/opt/RickWotbMods/lib/env/user.env
+    echo 'wotbPacks="$HOME/.local/share/Steam/steamapps/compatdata/444200/pfx/drive_c/users/steamuser/AppData/Local/wotblitz/packs/"' >>/opt/RickWotbMods/lib/env/user.env
   fi
   source /opt/RickWotbMods/lib/env/user.env
 }
