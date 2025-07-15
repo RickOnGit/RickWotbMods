153ec151469e61a9f34ee2c5ae11b53a2d8bce18
rick <riccardoguadagnini7@gmail.com>
2025-07-16 00:56:21 +0200

    Better previwe UI

--- lib/functions/preview.sh
+++ lib/functions/preview.sh
@@ -1,6 +1,8 @@
 function modPreview() {
   local file="$1"
   local category="$2"
+  BLUE='\033[0;34m' # Blue
+  NC='\033[0m'      # No Color
   elem=$(jq -r --arg category "$category" '.[$category][] | .name' $file)
 
   echo "$elem" >$filterFile
@@ -13,13 +15,13 @@ function modPreview() {
   while IFS= read -r mod; do
     site=$(jq -r --arg category "$category" --arg baseName "$selectedElem" --arg modName "$mod" ' .[$category][] | select(.name == $baseName) | .mods[] | select(.name == $modName) | .site' $file)
 
-    modLinks+=("• $(printf '\e]8;;%s\a%s\e]8;;\a' "$site" "$mod")")
+    modLinks+=("• $(printf '\e]8;;%s\a%s\e]8;;\a' "$site" "${BLUE}${mod}${NC}")")
   done <<<"$selectedMods"
 
   formattedLinks=$(printf "%s\n" "${modLinks[@]}")
   title=$(gum format -- "# Mods for $selectedElem" "(_ctrl+mouse click_)")
 
-  list=$(gum style --margin="1 1" --align="center" --border="none" "$(echo -e "$title\n\n$formattedLinks")")
+  list=$(gum style --border="rounded" --margin="1 1" --align="center" "$(echo -e "$title\n\n$formattedLinks")")
 
   echo "$list"
 }
