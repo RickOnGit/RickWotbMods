c5bff6c7a0be6f982fb6f114f374596dcaab01cb
rick <riccardoguadagnini7@gmail.com>
2025-08-03 20:52:59 +0200

    fix(app): New way to restore items
    Optimizing the code for doing backups of folders and files in two different ways avoiding unwanted files restoration in the game...

--- lib/functions/restore.sh
+++ lib/functions/restore.sh
@@ -1,22 +1,27 @@
 function restoreItems() {
   local file="$1"
-  formattedNames=() #UI decoration
   selectedArray=()
 
-  jq -r '.[] | .name' "$file" >$tmpFile
-  eval "selected=\$(cat \$tmpFile | gum filter --header \"Select item(s) to restore 📋\" --no-limit $gum_filter_prompt)"
+  jq -r '.[] | select(has("backupDir") or has("backupFiles")) | .name' "$file" >"$tmpRestore"
+  eval "selected=\$(cat \$tmpRestore | gum filter --header \"Select item(s) to restore 📋\" --no-limit $gum_filter_prompt)"
 
   while IFS= read -r name; do
     selectedArray+=("$name")
   done <<<"$selected"
 
   if [ -n "$selected" ]; then
-    >$tmpFile
+    >$tmpRestore
     for elem in "${selectedArray[@]}"; do
-      jq -r --arg mainName "$elem" '.[] | select(.name == $mainName) | .backupItems[]' "$file" >>$tmpFile
-      #will write all file names to restore from backup to wotbData
-      echo -e "${BLUE}${BOL}♻️ Restoring${NC} ${BOL}${ORANGE}$elem${NC}'s stock files..."
-      restore $tmpFile $elem
+      for type in "Dir" "Files"; do
+        jq -r --arg mainName "$elem" ".[] | select(.name == \$mainName) | .backup${type}[]" "$file" >>$tmpRestore 2>/dev/null
+
+        if [ -s "$tmpRestore" ]; then
+          echo -e "${BLUE}${BOL}♻️ Restoring${NC} ${BOL}${ORANGE}$elem${NC}'s stock files..."
+          restore "$tmpRestore" "$elem" "$type"
+        fi
+
+        >$tmpRestore
+      done
     done
   else
     return
@@ -26,15 +31,25 @@ function restoreItems() {
 function restore() {
   local file="$1"
   local name="$2"
+  local type="$3"
+
   while IFS= read -r fileName; do
-    gum spin -s "pulse" --spinner.foreground="208" --title "Restoring..." --title.foreground="245" -- rsync -a --include="*/" --include="*${fileName}*/" --include="*${fileName}*" --include="${fileName}/**" --exclude="*" "$wotbBackup" "$wotbData"
-  done <$file
-  >$tmpFile
-  echo -e "${BOL}${ORANGE}$elem${NC} restored ✅\n"
+    if [[ "$type" == "Files" ]]; then
+      includePattern="*${fileName}*"
+    else
+      includePattern="${fileName}/**"
+    fi
+
+    backupDir
+    gum spin -s "pulse" --spinner.foreground="208" --title "Restoring..." --title.foreground="245" -- rsync -a --include="*/" --include="$includePattern" --exclude="*" "$wotbBackup" "$wotbData"
+  done <"$file"
+
+  echo -e "♻️ ${BOL}${ORANGE}$name${NC} restored" >>$tmpLogs
+
 }
 
 function restoreGame() {
   echo -e "${BOLD}${GREEN}Restoring stock game files...${NC}"
   gum spin -s "pulse" --spinner.foreground="208" --title.foreground="245" --title "Restoring..." -- rsync -a "$wotbBackup" "$wotbData"
-  echo -e "${BOLD}${GREEN}Stock game files applied ✅${NC}\n"
+  echo -e "♻️ ${BOLD}${GREEN}Stock game files applied${NC}" >>$tmpLogs
 }
